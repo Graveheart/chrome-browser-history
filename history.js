cdd2fd485613ae7ecccc4a99c91a7adbea340d64
Graveheart <vbbanov@gmail.com>
2016-10-29 19:18:07 +0200

    feat(history.js): animate line hover

--- history.js
+++ history.js
@@ -1,3 +1,156 @@
+
+function button() {
+
+  var width = 960,
+    height = 500,
+    radius = 10,
+    padding = 10,
+    count = 0,
+    container = null,
+    text = null;
+
+  var defs = null,
+    gradient = null,
+    shadow = null,
+    cb = null,
+    rect = null;
+
+  function my() {
+
+    // set defaults
+    g = container || d3.select('svg').append('g')
+        .attr('class', 'button')
+        .attr('transform', 'translate(' + [width / 2, height / 2] + ")");
+    text = text || g.append('text').text('Hello, world!');
+
+    defs = g.append('defs');
+
+    var bbox = text.node().getBBox();
+    rect = g.append('rect')
+      .attr("x", bbox.x - padding)
+      .attr("y", bbox.y - padding)
+      .attr("width", bbox.width + 2 * padding)
+      .attr("height", bbox.height + 2 * padding)
+      .attr('rx', radius)
+      .attr('ry', radius)
+
+    addGradient(count);
+    addShadow(count);
+
+    rect.attr('fill', function () { return gradient ? 'url(#gradient' + count + ')' : 'steelblue'; })
+      .attr('filter', function() { return shadow ? 'url(#dropShadow' + count + ')' : null; })
+      .on('mouseover', brighten)
+      .on('mouseout', darken)
+      .on('mousedown', press)
+      .on('mouseup', letGo)
+
+    // put text on top
+    g.append(function() { return text.remove().node(); })
+
+    // TESTING -- SVG "use" element for testing dimensions of drop-shadow filter
+//    g.append('use').attr('xlink:href', '#shadowrect' + count)
+
+    return my;
+  }
+
+  function addGradient(k) {
+    gradient = defs.append('linearGradient')
+      .attr('id', 'gradient' + k)
+      .attr('x1', '0%')
+      .attr('y1', '0%')
+      .attr('x2', '0%')
+      .attr('y2', '100%');
+
+    gradient.append('stop')
+      .attr('id', 'gradient-start')
+      .attr('offset', '0%')
+
+    gradient.append('stop')
+      .attr('id', 'gradient-stop')
+      .attr('offset', '100%')
+  }
+
+  function addShadow(k) {
+    shadow = defs.append('filter')
+      .attr('id', 'dropShadow' + k)
+      .attr('x', rect.attr('x'))
+      .attr('y', rect.attr('y'))
+      .attr('width', rect.attr('width'))
+      .attr('height', rect.attr('height'))
+
+    // TESTING size of drop-shadow filter
+    defs.append('rect')
+      .attr('id', 'shadowrect' + k)
+      .attr('x', rect.attr('x'))
+      .attr('y', rect.attr('y'))
+      .attr('width', rect.attr('width'))
+      .attr('height', rect.attr('height'))
+
+    shadow.append('feGaussianBlur')
+      .attr('in', 'SourceAlpha')
+      .attr('stdDeviation', '3')
+
+    shadow.append('feOffset')
+      .attr('dx', '2')
+      .attr('dy', '4')
+
+    var merge = shadow.append('feMerge')
+
+    merge.append('feMergeNode')
+    merge.append('feMergeNode').attr('in', 'SourceGraphic');
+  }
+
+  function brighten() {
+    gradient.select('#gradient-start').classed('active', true)
+    gradient.select('#gradient-stop').classed('active', true)
+  }
+
+  function darken() {
+    gradient.select('#gradient-start').classed('active', false);
+    gradient.select('#gradient-stop').classed('active', false);
+  }
+
+  function press() {
+    if (typeof cb === 'function') cb();
+    if (shadow) shadow.select('feOffset')
+      .attr('dx', '0.5')
+      .attr('dy', '1')
+  }
+
+  function letGo() {
+    if (shadow) shadow.select('feOffset')
+      .attr('dx', '2')
+      .attr('dy', '4')
+  }
+
+  my.container = function(_) {
+    if (!arguments.length) return container;
+    container = _;
+    return my;
+  };
+
+  my.text = function(_) {
+    if (!arguments.length) return text;
+    text = _;
+    return my;
+  };
+
+  my.count = function(_) {
+    if (!arguments.length) return count;
+    count = _;
+    return my;
+  };
+
+  my.cb = function(_) {
+    if (!arguments.length) return cb;
+    cb = _;
+    return my;
+  };
+
+  return my;
+}
+
+
 const dummyDataset = {
   '1': {
     id: '1',
@@ -105,9 +258,19 @@ const renderDataset = dataset => {
     .attr('y2',10)
     .attr("stroke",visit => assignColor(visit.id))
     .attr("stroke-width", 10)
+    .attr('visit_id',visit => visit.id)
     .on('mouseover',handleMouseOver)
     .on('mouseout',handleMouseOut);
-
+  // var g_button = g.append('g')
+  //   .attr('class', 'button')
+  //   .attr('transform', 'translate(' + [480 / 2, 240 / 2] +')')
+  // var text = g_button.append('text')
+  //   .text('Click me');
+  // button()
+  //   .container(g_button)
+  //   .text(text)
+  //   .count(0)
+  //   .cb(function() { console.log("I've been clicked!") })();
   g
     .append('text')
     .style('fill', 'black')
@@ -153,9 +316,27 @@ const switchLine = (visitID,display) => {
 
     // only show line if parent exists
     if(visitParentID != undefined){
-        d3.select(`polyline[visit_id='${visitID}']`).attr('display',displayAttr)
-        switchLine(visitParentID,display) // switch parent's line as well
+      d3.select(`polyline[visit_id='${visitID}']`).attr('display',displayAttr);
+      switchLine(visitParentID,display) // switch parent's line as well
     }
+    var lineHovered = d3.select(`line[visit_id='${visitID}']`);
+    var attributes = {};
+    if (display) {
+      attributes = {
+        "stroke-width": 50,
+        "y1": 30,
+        "y2": 30
+      };
+    }
+    else {
+      attributes = {
+        "stroke-width": 10,
+        "y1": 10,
+        "y2": 10
+      };
+    }
+    lineHovered.transition()
+      .duration(1000).attr(attributes);
 }
 
 renderDataset(dummyDataset);
